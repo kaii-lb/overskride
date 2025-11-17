a99f9a9458b48d6fdb5705d92e074a6f89d0007b
kaii_lb <imkaiilb@gmail.com>
2025-11-17 21:08:57 +0200

    build: update workflow.yml to try and support rust edition2024

--- make-pkg.sh
+++ make-pkg.sh
@@ -6,19 +6,20 @@ mkdir -p /home/$USER/Projects/overskride/build/src/release/package
 cd /home/$USER/Projects/overskride/build/src/release/package
 
 mkdir -p usr/bin
+mkdir -p usr/share/overskride
 mkdir -p usr/share/applications
 mkdir -p usr/share/appdata
 mkdir -p usr/share/glib-2.0/schemas
 mkdir -p usr/share/icons/hicolor/scalable/apps
 mkdir -p usr/share/icons/hicolor/symbolic/apps
 
-cp ../overskride usr/bin/overskride
+cp ../../overskride usr/bin/overskride
 cp ../../overskride.gresource usr/share/overskride/
 cp ../../../data/io.github.kaii_lb.Overskride.desktop usr/share/applications/
 cp ../../../data/io.github.kaii_lb.Overskride.appdata.xml usr/share/appdata/
-cp /home/kaii/Projects/overskride/data/io.github.kaii_lb.Overskride.gschema.xml usr/share/glib-2.0/schemas/
-cp /home/kaii/Projects/overskride/data/icons/hicolor/scalable/apps/io.github.kaii_lb.Overskride.svg usr/share/icons/hicolor/scalable/apps/
-cp /home/kaii/Projects/overskride/data/icons/hicolor/symbolic/apps/io.github.kaii_lb.Overskride-symbolic.svg usr/share/icons/hicolor/symbolic/apps/
+cp /home/$USER/Projects/overskride/data/io.github.kaii_lb.Overskride.gschema.xml usr/share/glib-2.0/schemas/
+cp /home/$USER/Projects/overskride/data/icons/hicolor/scalable/apps/io.github.kaii_lb.Overskride.svg usr/share/icons/hicolor/scalable/apps/
+cp /home/$USER/Projects/overskride/data/icons/hicolor/symbolic/apps/io.github.kaii_lb.Overskride-symbolic.svg usr/share/icons/hicolor/symbolic/apps/
 
 cd ..
 
