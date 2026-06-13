27b61b93460f683ff5c826dfe4829560c4b0560e
kaii_lb <imkaiilb@gmail.com>
2026-06-13 14:12:39 +0300

    feature: upgrade to gnome platform 49

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -5,6 +5,6 @@ rm -rf overskride-flatpak
 mkdir flatpak-build/
 mkdir overskride-flatpak
 
-flatpak-builder flatpak-build/ io.github.kaii_lb.Overskride.json --force-clean
+flatpak-builder --installation=user flatpak-build/ io.github.kaii_lb.Overskride.json --force-clean
 flatpak build-export overskride-flatpak flatpak-build main
 flatpak build-bundle overskride-flatpak/ overskride.flatpak io.github.kaii_lb.Overskride main
